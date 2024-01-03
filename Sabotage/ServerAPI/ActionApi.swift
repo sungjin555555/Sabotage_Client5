8078fd5dba64b415696f42e8529933c2eb29b80c
haram22 <kimhr9889@handong.ac.kr>
2024-01-03 13:41:02 +0900

    [Feat] connet server _ PostActionItem

--- Sabotage/ServerAPI/ActionApi.swift
+++ Sabotage/ServerAPI/ActionApi.swift
@@ -16,28 +16,21 @@ func actionPostRequest(with category: String, content: String) {
     }
     print("✅ Valid URL = \(url)")
     print("🥹 userId = \(userId)")
-    // request 생성하기
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
-    // json 형식으로 데이터 전송할 것임
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    
-    // POST로 요청할 경우 : json 형식으로 데이터 넘기기
     let body:[String: AnyHashable] = [
         "category": category,
         "content": content
     ]
     request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
     
-    // data task 생성하기
     let task = URLSession.shared.dataTask(with: request) { data, _, error in
-        // 응답 처리하기
         if let error = error {
             print("🚨 Error: \(error.localizedDescription)")
             return
         }
         
-        // 데이터가 비어있는지 확인
         guard let data = data, !data.isEmpty else {
             print("✅ [actionPostRequest] No data returned from the server")
             return
